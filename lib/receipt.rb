ece53591a2dd2e95701e033a73f1b1c78756057f
Neal Deters <nealdeters@gmail.com>
2022-04-29 17:34:10 -0500

    chore: cleanup receipt

--- lib/receipt.rb
+++ lib/receipt.rb
@@ -1,5 +1,4 @@
 class Receipt
-  attr_reader :receipt
   def initialize(cart)
     raise ArgumentError, 'Invalid Cart.' if !cart.is_a?(Cart)
     
@@ -9,10 +8,14 @@ class Receipt
   def print
     @cart.products.each do |product|
       price = ( (product.price + product.tax) * product.quantity).round(2)
-      puts "#{product.quantity} #{ product.imported ? 'imported ' + product.name: product.name }: #{sprintf("%.2f", price)}" 
+      puts "#{product.quantity} #{ product.imported ? 'imported ' + product.name: product.name }: #{clean_price(price)}" 
     end
 
-    puts "Sales Tax: #{sprintf("%.2f", @cart.total_tax)}"
-    puts "Total: #{sprintf("%.2f", @cart.total_price)}"
+    puts "Sales Tax: #{clean_price(@cart.total_tax)}"
+    puts "Total: #{clean_price(@cart.total_price)}"
+  end
+
+  def clean_price(price)
+    sprintf("%.2f", price)
   end
 end
